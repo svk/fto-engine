371625ce6f7da0b350befc6e9057f0ea5839b0df
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2010-12-21 03:33:00 +0100

    Snarky angle lib

---
+++ hexy/Angelic.h
@@ -0,0 +1,23 @@
+#ifndef H_ANGELIC
+#define H_ANGELIC
+
+namespace Angelic {
+
+    struct Coordinate {
+        int x, y;
+        Coordinate(int,int);
+
+        bool operator>(const Coordinate&) const;
+        bool operator<(const Coordinate&) const;
+        bool operator>=(const Coordinate&) const;
+        bool operator<=(const Coordinate&) const;
+        bool operator==(const Coordinate&) const;
+
+        private:
+            int quadrant(void) const;
+            int compare(const Coordinate&) const;
+    };
+
+};
+
+#endif
