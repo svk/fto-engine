f3c68ddfcb4f9b70220959827b2a9e2d910a9fbe
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-01-02 17:40:25 +0100

    Tests modified, bug id'd

--- sfml/test-hexfml.cpp
+++ sfml/test-hexfml.cpp
@@ -1,27 +1,69 @@
 #include "hexfml.h"
 
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
+
+#include <stdexcept>
 
 int main(int argc, char *argv[]) {
     using namespace std;
 
-    const int sx = atoi(argv[1]),
-              sy = atoi(argv[2]);
-
     ScreenGrid grid ( "./data/hexproto1.png" );
 
-    for(int row=0;row<24;row++) {
-        for(int col=0;col<79;col++) {
-            int x = col, y = row;
-            grid.screenToHex( x, y, sx, sy );
-            if( x == 0 && y == -2 ) {
-                cout << 'x';
-            } else {
-                cout << ' ';
-            }
+    sf::RenderWindow win ( sf::VideoMode(800,600,32),
+                           "521 HexFML" );
+    sf::Clock clock;
 
+    sf::Image hexBorderImage;
+    if( !hexBorderImage.LoadFromFile( "./data/hexborder1.png" ) ) {
+        throw std::runtime_error( "unable to load hex border" );
+    }
+    sf::Sprite hexBorder;
+    hexBorder.SetImage( hexBorderImage );
+
+    int currentHexX = -1000,
+        currentHexY = -1000;
+
+    while( win.IsOpened() ) {
+        sf::Event ev;
+        const double dt = clock.GetElapsedTime();
+        clock.Reset();
+
+        while( win.GetEvent( ev ) ) switch( ev.Type ) {
+            case sf::Event::Resized:
+                win.SetView( sf::View( sf::FloatRect( 0,
+                                                      0,
+                                                      ev.Size.Width,
+                                                      ev.Size.Height ) ) );
+                break;
+            case sf::Event::Closed:
+                win.Close();
+                break;
+            case sf::Event::MouseMoved:
+                {
+                    int x = win.GetInput().GetMouseX(),
+                        y = win.GetInput().GetMouseY();
+                    grid.screenToHex( x, y, 0, 0 );
+                    int hx = x, hy = y;
+                    hy += 2; // HAX should be in the grid code
+                    grid.hexToScreen( hx, hy );
+                    cout << x << " " << y << ", ";
+                    cout << hx << " " << hy << endl;
+                    currentHexX = hx;
+                    currentHexY = hy;
+                }
+                break;
         }
-        cout << endl;
+
+        win.Clear( sf::Color(0,0,0) );
+
+        hexBorder.SetPosition( currentHexX,
+                               currentHexY );
+        win.Draw( hexBorder );
+
+        win.Display();
     }
 
     return 0;
