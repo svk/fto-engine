03cc91481cf37aee0903efe92578bfefcf35bccb
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-01-07 05:21:27 +0100

    At least this compiles

---
+++ sfml/sftools.cpp
@@ -0,0 +1,95 @@
+#include "sftools.h"
+
+SfmlEventHandler::SfmlEventHandler(void) :
+    next ( 0 )
+{
+}
+
+void SfmlEventHandler::setNextEventHandler(SfmlEventHandler *next_) {
+    next = next_;
+}
+
+bool SfmlEventHandler::delegate(const sf::Event& event) {
+    if( next ) return next->handleEvent( event );
+    return false;
+}
+
+bool CompositeEventHandler::handleEvent(const sf::Event& ev) {
+    for(HandlerList::iterator i = handlers.begin(); i != handlers.end(); i++) {
+        if( (*i)->handleEvent( ev ) ) return true;
+    }
+    return delegate( ev );
+}
+
+CompositeEventHandler::CompositeEventHandler(void) {
+}
+
+CompositeEventHandler::~CompositeEventHandler(void) {
+    for(HandlerList::iterator i = handlers.begin(); i != handlers.end(); i++) {
+        delete *i;
+    }
+}
+
+void SfmlApplication::processIteration(void) {
+    sf::Event ev;
+    double dt = clock.GetElapsedTime();
+    clock.Reset();
+    if( dt > 0 ) {
+        tick( dt );
+    }
+    while( window.GetEvent( ev ) ) {
+        handleEvent( ev );
+    }
+    if( currentScreen ) {
+        currentScreen->draw( window );
+    }
+    window.Display();
+}
+
+void SfmlApplication::run(void) {
+    while( window.IsOpened() ) {
+        processIteration();
+    }
+}
+
+void CompositeEventHandler::adoptHandler(SfmlEventHandler* handler) {
+    handlers.push_back( handler );
+}
+
+void SfmlApplication::resize(int width_, int height_) {
+    width = width_;
+    height = height_;
+    currentScreen->resize(width, height);
+}
+
+bool SfmlApplication::handleEvent(const sf::Event& ev) {
+    switch( ev.Type ) {
+        case sf::Event::Resized:
+            resize( ev.Size.Width, ev.Size.Height );
+            break;
+        case sf::Event::Closed:
+            window.Close();
+            break;
+        default:
+            return delegate(ev);
+    }
+}
+
+void SfmlApplication::setScreen(SfmlScreen* screen) {
+    currentScreen = screen;
+    currentScreen->resize( width, height );
+    setNextEventHandler( currentScreen );
+}
+
+SfmlApplication::SfmlApplication(std::string title, int width, int height) :
+    width (width),
+    height (height),
+    clock (),
+    window ( sf::VideoMode( width, height, 32 ), title ),
+    currentScreen ( 0 )
+{
+}
+
+SfmlApplication::~SfmlApplication(void) {
+}
+
--- sfml/sftools.h
+++ sfml/sftools.h
@@ -1,6 +1,7 @@
 #ifndef H_SFTOOLS
 #define H_SFTOOLS
 
+#include <SFML/Graphics.hpp>
 
 #include <string>
 #include <map>
@@ -31,6 +32,75 @@ class ResourceManager {
         T& operator[](const std::string& str) {
             return *resources[ str ];
         }
+};
+
+class SfmlEventHandler {
+    private:
+        SfmlEventHandler *next;
+    
+    protected:
+        bool delegate(const sf::Event&);
+        
+    public:
+        SfmlEventHandler(void);
+
+        void setNextEventHandler(SfmlEventHandler*);
+        virtual bool handleEvent(const sf::Event&) = 0;
+};
+
+class CompositeEventHandler : public SfmlEventHandler {
+    // For a whole bunch of event handlers where
+    // the internal ordering doesn't matter or
+    // will never change.
+    private:
+        // owned pointers
+        typedef std::vector<SfmlEventHandler*> HandlerList;
+        HandlerList handlers;
+
+    public:
+        CompositeEventHandler(void);
+        ~CompositeEventHandler(void);
+
+        void adoptHandler(SfmlEventHandler*);
+
+        bool handleEvent(const sf::Event&);
+};
+
+class SfmlApplication;
+
+class SfmlScreen : public SfmlEventHandler {
+    private:
+        SfmlApplication& app;
+
+    public:
+        virtual void draw(sf::RenderWindow&) = 0;
+        virtual bool handleEvent(const sf::Event&) = 0;
+        virtual void resize(int,int) = 0;
+};
+
+class SfmlApplication : public SfmlEventHandler {
+    private:
+        int width, height;
+
+        sf::Clock clock;
+        sf::RenderWindow window;
+        sf::View mainView; // work out role
+        SfmlScreen *currentScreen;
+
+        void processIteration(void);
+        void resize(int,int);
+
+    public:
+        SfmlApplication(std::string, int, int);
+        ~SfmlApplication();
+
+        void run(void);
+
+        virtual void tick(double) {};
+
+        bool handleEvent(const sf::Event&);
+
+        void setScreen(SfmlScreen*);
 };
 
 #endif
