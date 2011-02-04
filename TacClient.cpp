60d0b4c4a12a4e477e75b147ccd6c3f7544b3f17
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-02-04 10:47:43 +0100

    Lots of high-concept untested code

---
+++ TacClient.cpp
@@ -0,0 +1,281 @@
+#include "TacClient.h"
+
+#include <cassert>
+
+namespace Tac {
+
+const double MovementAnimationDuration = 0.15;
+
+ClientUnit::ClientUnit(int id, ClientUnitType& unitType, int team, int owner) :
+    id ( id ),
+    unitType ( unitType ),
+    team ( team ),
+    owner ( owner ),
+    state ( LIVING ),
+    layer ( 0 ),
+    x ( 0 ),
+    y ( 0 ),
+    hasPosition ( false )
+{
+}
+
+void ClientUnit::setLiving(bool val) {
+    state = val ? LIVING : DEAD;
+}
+
+void ClientUnit::setPosition(int x_, int y_) {
+    hasPosition = true;
+    x = x_;
+    y = y_;
+}
+
+void ClientUnit::setNoPosition(void) {
+    hasPosition = false;
+    x = y = 0;
+}
+
+
+int ClientUnit::getId(void) const {
+    return id;
+}
+
+void ClientUnitManager::adopt(ClientUnit* unit) {
+    ClientUnit *prev = (*this)[unit->getId()];
+    assert( !prev );
+    if( prev ) {
+        delete prev;
+    }
+    units[ unit->getId() ] = unit;
+}
+
+ClientUnitManager::~ClientUnitManager(void) {
+    for(std::map<int,ClientUnit*>::iterator i = units.begin(); i != units.end(); i++) {
+        delete i->second;
+    }
+    units.clear();
+}
+
+ClientUnit* ClientUnitManager::operator[](int id) const {
+    std::map<int,ClientUnit*>::const_iterator i = units.find( id );
+    if( i == units.end() ) return 0;
+    return i->second;
+}
+
+ClientTile::ClientTile(void) :
+    tileType ( 0 ),
+    highlight ( NONE ),
+    inactive ( true )
+{
+    clearUnits();
+}
+
+void ClientTile::setHighlight( ClientTile::Highlight highlight_ ) {
+    highlight = highlight_;
+}
+
+void ClientTile::setActive(void) {
+    inactive = false;
+}
+
+int ClearUnit::clearUnitById(int id) {
+    for(int i=0;i<UNIT_LAYERS;i++) if( unitId[i] == id ) {
+        unitId[i] = 0;
+        return i;
+    }
+    return -1;
+}
+
+void ClientTile::clearUnits(void) {
+    for(int i=0;i<UNIT_LAYERS;i++) {
+        unitId[i] = INVALID_ID;
+    }
+}
+
+void ClientTile::setInactive(void) {
+    inactive = true;
+    clearUnits();
+}
+
+void ClientMap::updateActive(const HexRegion& visible) {
+    for(int r=0;r<radius;r++) for(int i=0;i<6;i++) for(int j=0;j<r;j++) {
+        int x, y;
+        cartesianiseHexCoordinate( i, j, r, x, y );
+        if( !visible.contains( x,y ) ) {
+            tiles.get(x,y).setInactive();
+        } else {
+            tiles.get(x,y).setActive();
+        }
+    }
+}
+
+bool ClientUnit::getPosition(int& ox, int& oy) {
+    if( hasPosition ) {
+        ox = x;
+        oy = y;
+    }
+    return hasPosition;
+}
+
+void ClientMap::moveUnit(int id,int dx,int dy) {
+    ClientUnit *unit = units[id];
+    int lx, ly, layer;
+    assert( unit );
+    if( !unit->getPosition( lx, ly ) ) {
+        throw std::logic_error( "unit cannot be moved without being placed" );
+    }
+    layer = unit->leaveTile( tiles.get( lx, ly ) );
+    placeUnitAt( id, lx + dx, ly + dy, layer );
+}
+
+void ClientMap::placeUnitAt(int id,int x,int y,int layer) {
+    ClientUnit *unit = units[id];
+    int lx, ly;
+    assert( unit );
+    if( unit->getPosition( lx, ly ) ) {
+        unit->leaveTile( tiles.get( lx, ly ) );
+    }
+    unit->enterTile( tiles.get( x, y ), layer );
+}
+
+void ClientUnit::enterTile(ClientTile& tile, int layer) {
+    tile.setUnit( id, layer );
+}
+
+int ClientUnit::leaveTile(ClientTile& tile) {
+    return tile.clearUnitById( id );
+}
+
+void ClientTile::setUnit(int id, int layer) {
+    assert( layer >= 0 && layer < UNIT_LAYERS );
+    unitId[ layer ] = id;
+}
+
+ClientMap::ClientMap(int radius) :
+    radius ( radius )
+    tiles ( radius ),
+    units (),
+    animatedUnit ( 0 )
+{
+}
+
+bool LineCurveAnimation::done(void) {
+    return phase >= duration;
+}
+
+void LineCurveAnimation::get(double& x, double& y) const {
+    if( phase <= 0.0 ) {
+        x = x0;
+        y = y0;
+    } else if( phase >= 1.0 ) {
+        x = x1;
+        y = y1;
+    } else {
+        x = (x1-x0) * (phase/duration);
+        y = (y1-y0) * (phase/duration);
+    }
+}
+
+void ClientUnit::animate(double dt) {
+    if( curveAnim ) {
+        curveAnim->animate( dt );
+    }
+}
+
+bool ClientUnit::done(void) const {
+    if( curveAnim ) {
+        return curveAnim->done();
+    }
+    return true;
+}
+
+void ClientUnit::completedAnimation(void) {
+    delete curveAnim;
+    curveAnim = 0;
+}
+
+void LineCurveAnimation::animate(double dt) {
+    phase += dt;
+}
+
+LineCurveAnimation::LineCurveAnimation(double duration, double x0, double y0, double x1, double y1) :
+    duration ( duration ),
+    x0 ( x0 ),
+    y0 ( y0 ),
+    x1 ( x1 ),
+    y1 ( y1 ),
+    phase ( 0.0 )
+{
+}
+
+void ClientMap::animate(double dt) {
+    if( animatedUnit ) {
+        animatedUnit->animate( dt );
+        if( animatedUnit->done() ) {
+            animatedUnit->completedAnimation();
+            animatedUnit = 0;
+        }
+    }
+}
+
+ClientActionQueue::~ClientActionQueue(void) {
+    while( !actions.empty() ) {
+        delete actions.pop();
+    }
+}
+
+bool ClientMap::shouldBlock(void) const {
+    return isInBlockingAnimation();
+}
+
+bool ClientMap::isInBlockingAnimation(void) const {
+    return animatedUnit != 0;
+}
+
+void ClientMap::adoptUnit(ClientUnit* unit) {
+    units.adopt( unit );
+}
+
+void ClientMap::queueAction(const ClientAction& action) {
+    caq.adopt( action.duplicate() );
+}
+
+void ClientMap::processActions(void) {
+    while( !shouldBlock() && !caq.empty() ) {
+        ClientAction *action = caq.pop();
+        (*action)();
+    }
+}
+
+bool ClientActionQueue::empty(void) const {
+    return actions.empty();
+}
+
+void ClientUnit::getCenterOffset(double& ox, double& oy) const {
+    if( curveAnim ) {
+        curveAnim->get( ox, oy );
+    } else {
+        ox = oy = 0;
+    }
+}
+
+void ClientUnit::startMovementAnimation(int dx,int dy) {
+    if( curveAnim ) {
+        delete curveAnim;
+    }
+    curveAnim = new LineCurveAnimation( MovementAnimationDuration,
+                                        0, 0,
+                                        dx, dy );
+}
+
+ClientUnit* ClientMap::getUnitById(int id) {
+    return units[id];
+}
+
+void MovementAnimationCAction::operator() const {
+    ClientUnit *unit = cmap.getUnitById(id);
+    if( unit ) {
+        unit->startMovementAnimation( dx, dy );
+    }
+}
+
+};
---
+++ TacClient.h
@@ -0,0 +1,256 @@
+#ifndef H_TAC_CLIENT
+#define H_TAC_CLIENT
+
+#define INVALID_ID 0
+#define UNIT_LAYERS 2
+
+namespace Tac {
+
+// for now we have universal mobility; this
+// will change -- most obvious exception
+// flyers, water.
+// client must know the unit type x tile type
+// -> energy cost function, and the energy
+// capacity for each unit. This is because
+// simple pathfinding should be integrated
+// into the UI and should be done without
+// delay.
+
+
+// impassable but clear: water or glass walls
+// passable but not clear: ? (closed doors, in a sense)
+
+// door is probably its own mobility/opacity type;
+// passability/opacity depends on state
+// opening a door is an ACTION (like an attack)
+
+namespace Type {
+    enum Mobility {
+        FLOOR,
+        WALL
+    };
+
+    enum Opacity {
+        CLEAR,
+        BLOCK
+    };
+};
+
+class CurveAnimation {
+    public:
+        virtual ~CurveAnimation(void) {}
+        virtual void animate(double) = 0;
+        virtual bool done(void) const = 0;
+        virtual void get(double&,double&) const = 0;
+}
+
+class LineCurveAnimation {
+    private:
+        const double duration;
+        double x0, y0, x1, y1;
+        double phase;
+
+    public:
+        void animate(double);
+        bool done(void) const;
+        void get(double&,double&) const;
+};
+
+struct ClientUnitType {
+    int spriteno;
+};
+
+struct ClientTileType {
+    int spriteno;
+    Mobility mobility;
+    Opacity opacity;
+    bool border;
+    int baseCost;
+
+    bool mayTraverse(ClientUnitType&,int&) const;
+};
+
+class ClientTile;
+
+class ClientUnit {
+    private:
+        int id; // randomized so client can't tell how many units there are etc.
+                // this can be FAKED in the case of e.g. hallucination, disguise
+        ClientUnitType& unitType;
+        int team;
+        int owner;
+        enum State {
+            LIVING,
+            DEAD
+        } state; // this might not be accurate -- illusions etc
+
+        int x, y;
+        bool hasPosition;
+
+        CurveAnimation *curveAnim;
+
+    public:
+        ClientUnit(int, ClientUnitType&, int, int);
+
+        void getCenterOffset(double&, double&) const;
+
+        int getId(void) const;
+
+        void setLiving(bool);
+        void setPosition(int,int);
+        void setNoPosition(void);
+
+        void enterTile(ClientTile&, int);
+        void leaveTile(ClientTile&);
+        void move(int,int);
+
+        bool getPosition(int&, int&);
+
+        void animate(double);
+        bool done(void) const;
+        void completedAnimation(void);
+
+        void startMovementAnimation(int,int);
+};
+
+class ClientUnitManager {
+    private:
+        std::map<int,ClientUnit*> units;
+
+    public:
+        ~ClientUnitManager(void);
+
+        void adopt(ClientUnit*);
+
+        ClientUnit* operator[](int) const; // may return 0!
+};
+
+class ClientTile {
+    public:
+        enum Highlight {
+            NONE = 0,
+            MOVE_ZONE,
+            ATTACK_ZONE
+        };
+
+    private:
+        ClientTileType *tileType; // 0 for unknown
+        Highlight highlight;
+        bool inactive; // show fog of war and don't trust completely
+
+        int unitId[ UNIT_LAYERS ];
+
+        void clearUnits(void);
+
+    public:
+        void setHighlight( Highlight );
+
+        int clearUnitById(int);
+        void setUnit(int, int);
+
+        void setActive(void);
+        void setInactive(void);
+
+        bool getActive(void) const { return !inactive; }
+};
+
+// client actions take time to execute
+// (because they need to be presented visually)
+// so we need to represent them and queue them up
+// it is ALSO desirable that we add some rewind/f-f
+// functionality -- required at least to be able
+// to replay the last turn
+// sane way: save a snapshot each turn
+// full client action queue should be enough to
+// reconstruct the entire game, but not all clients
+// get all actions. (when viewing a replay special
+// care must be taken re: _illusory_ units -- both
+// the illusion and the reality should be viewable,
+// depending on the perspective chosen)
+// the ones units sent from the server may be larger
+// than this, especially since we split actions into
+// "cosmetic" and "non-cosmetic" actions -- so
+// (move 1337 3 1) is translated to MovementAnimation,
+// MoveUnit, (StopAnimation).
+
+// actions should be _POD objects.
+// use coordinates and IDs, never references or pointers
+
+class ClientAction {
+    public:
+        virtual ~ClientAction(void) {}
+
+        virtual ClientAction* duplicate(void) const = 0;
+        virtual void operator() const = 0;
+        virtual bool isCosmetic(void) const = 0;
+};
+
+class ClientActionQueue {
+    private:
+        std::queue< ClientAction* > actions;
+
+    public:
+        ~ClientActionQueue(void);
+
+        bool empty(void) const;
+
+        void adopt(ClientAction*);
+        ClientAction* pop(void);
+};
+
+class ClientMap;
+
+struct MovementAnimationCAction : public ClientAction {
+    ClientMap& cmap;
+    int unitId;
+    int dx, dy;
+
+    MovementAnimationCAction(ClientMap& cmap, int unitId, int dx, int dy) :
+        cmap( cmap ), unitId( unitId ), dx ( dx ), dy ( dy )
+    {
+    }
+
+    ClientAction* duplicate(void) { return new MovementAnimationCAction( cmap, unitId, dx, dy ); }
+
+    void operator() const;
+
+    bool isCosmetic(void) const { return true; }
+};
+
+
+class ClientMap {
+    private:
+        int radius;
+        HexMap<ClientTile> tiles;
+        ClientUnitManager units;
+
+        ClientActionQueue caq;
+
+        ClientUnit *animatedUnit;
+
+        bool shouldBlock(void) const;
+        bool isInBlockingAnimation(void) const;
+
+    public:
+        ClientMap(int);
+
+        void updateActive(const HexRegion&);
+
+        void adoptUnit(ClientUnit*);
+
+        // low-level movement!
+        void placeUnitAt(int,int,int,int);
+        void moveUnit(int,int,int);
+
+        void animate(double dt);
+
+        void queueAction(const ClientAction&);
+        void processActions(void);
+
+        ClientUnit* getUnitById(int);
+};
+
+
+};
+
+#endif
